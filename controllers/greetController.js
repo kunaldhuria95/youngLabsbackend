02e32188b043c6db9f104820256acb8d45a0dbe8
kunal dhuria <kunaldhuria95@gmail.com>
2025-03-07 16:40:57 +0530

    typos fixed

--- controllers/greetController.js
+++ controllers/greetController.js
@@ -1,11 +1,16 @@
 import { StatusCodes } from "http-status-codes";
 
-const getGreeting = (req, res) => {
-    const name = req.query.name;
-    if (!name) {
-        return res.status(StatusCodes.BAD_REQUEST).json({ "error": "Name is required" })
+const getGreeting = (req, res, next) => {
+    try {
+        const name = req.query.name;
+        if (!name) {
+            return res.status(StatusCodes.BAD_REQUEST).json({ "error": "Name is required" })
+        }
+        res.status(StatusCodes.OK).json({ "message": `Hello, ${name}! Welcome To YoungLabs.` })
+    } catch (error) {
+        next(error)
     }
-    res.status(StatusCodes.OK).json({ "message": `Hello, ${name}! Welcome To YoungLabs.` })
+
 }
 
 export { getGreeting }
